b9840404d9c5c630bf05ea5601f501251e74af2a
beingzero <beingzeroin@gmail.com>
2020-07-24 07:58:48 +0530

    Usage of id in CSS and JS

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,7 @@
 function runOnClick(){
     console.log("You clicked button");
-    alert("You clicked button");
+    //alert("You clicked button");
+
+    var p1 = document.getElementById("para1");
+    p1.innerHTML = "This is new content of paragraph from Javascript";
 }

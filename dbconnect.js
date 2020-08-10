6c30feab9fa42979d825e93b9a5eb85f28bdf63e
beingzero <beingzeroin@gmail.com>
2020-08-10 07:09:03 +0530

    Exporint JSON object having functions

--- dbconnect.js
+++ dbconnect.js
@@ -1,27 +1,28 @@
 var mongoose = require('mongoose');
 
-module.exports.connect = function()
+module.exports = 
 {
-    // srv+mongodb://
-    var connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017/userdb';
-    var optionsJSON = {   useNewUrlParser: true, useUnifiedTopology: true };
-    mongoose.connect(connectionString, optionsJSON);
+    connect : function(){
+        // srv+mongodb://
+        var connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017/userdb';
+        var optionsJSON = {   useNewUrlParser: true, useUnifiedTopology: true };
+        mongoose.connect(connectionString, optionsJSON);
 
-    var connectionObj = mongoose.connection;
+        var connectionObj = mongoose.connection;
 
-    connectionObj.on('connected', function(){
-        console.log("CONNECTED TO DB");
-    })
+        connectionObj.on('connected', function(){
+            console.log("CONNECTED TO DB");
+        })
 
-    connectionObj.on('disconnected', function(){
-        console.log("DISCONNECTED FROM DB");
-    })
+        connectionObj.on('disconnected', function(){
+            console.log("DISCONNECTED FROM DB");
+        })
 
-    connectionObj.on('error', function(err){
-        console.log("ERROR: "+err);
-    })
-}
-
-module.exports.disconnect = function(){
-    mongoose.disconnect();   
+        connectionObj.on('error', function(err){
+            console.log("ERROR: "+err);
+        })
+    },
+    disconnect : function(){
+        mongoose.disconnect();   
+    }
 }
